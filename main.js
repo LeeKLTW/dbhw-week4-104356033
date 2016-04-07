f203afa00729223984de0257fe7b3f07cf31708f
KunLinLee <LeeKLTW@gmail.com>
2016-04-07 19:41:28 +0800

    init

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5";
-
-//http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=bf073841-c734-49bf-a97f-3757a6013812
-
-function getData() {
+//var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=bf073841-c734-49bf-a97f-3757a6013812";
+function getData(cb) {
   $.ajax({
     url : API_PATH,
-    dataType:"jsonp",
-    type: "GET",
+    method : "GET",
     success : function(data) {
-      console.log(data);
+      cb(null, data);
     },
     error : function(err) {
-      console.log(err);
+      cb(err);
     }
   });
 }
