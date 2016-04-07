4cabfc9b2930bd34914a1a4fbdb841ce92d199b3
KunLinLee <LeeKLTW@gmail.com>
2016-04-07 18:32:21 +0800

    getData func modified

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
 
-function getData(cb) {
+function getData() {
   $.ajax({
     url : API_PATH,
-    method : "GET",
+    type: "GET",
     success : function(data) {
-      cb(null, data);
+      alert(data);
     },
     error : function(err) {
-      cb(err);
+      alert(err);
     }
   });
 }
