69f979aecad2d485007d7d5b8b2394a69e3b3e24
KunLinLee <LeeKLTW@gmail.com>
2016-04-07 20:49:47 +0800

    //

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=0a1e7957-7699-452c-8020-bf11a5408fc8";
+// var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=0a1e7957-7699-452c-8020-bf11a5408fc8";
 function getData(cb) {
   $.ajax({
     url : API_PATH,
