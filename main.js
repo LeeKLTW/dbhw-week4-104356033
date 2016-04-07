88cb7c22f76b544b307542ac6836c9f4aae0bdea
KunLinLee <LeeKLTW@gmail.com>
2016-04-07 20:47:48 +0800

    change url

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=0a1e7957-7699-452c-8020-bf11a5408fc8";
 function getData(cb) {
   $.ajax({
     url : API_PATH,
